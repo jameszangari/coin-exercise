c38def9cb37b580261507be1b597df58e8133c00
James Cliff <me@jamescliff.com>
2022-07-07 06:59:21 -0400

    feat: updates

--- src/App.tsx
+++ src/App.tsx
@@ -20,14 +20,29 @@ function App() {
   }, []);
   console.log(coins);
 
+  // Search
   const [search, setSearch] = useState("");
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.currentTarget.value);
   };
-
   const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
+
+  // Compare
+  const [baseSelected, setBaseSelected] = useState("");
+  const handleBaseSelected = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBaseSelected(e.currentTarget.value);
+  };
+  const baseSelectedCoins = coins.filter((coin) =>
+    coin.name.includes(baseSelected)
+  );
+
+  const [selected, setSelected] = useState("");
+  const handleSelected = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.currentTarget.value);
+  };
+  const selectedCoins = coins.filter((coin) => coin.name.includes(selected));
 
   if (isLoading)
     return (
@@ -37,9 +52,96 @@ function App() {
     );
   if (!coins) return <p className="text-center">No data</p>;
   return (
-    <div className="m-4 max-w-7xl mx-auto">
-      <div className="flex flex-row items-center">
-        <h1 className="text-xl">Search currency</h1>
+    <div className="m-4 p-4 max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-3xl text-center">Compare Currencies</h1>
+        <div className="flex flex-row justify-center">
+          <select
+            value={baseSelected}
+            name="baseSelected"
+            id="baseSelected"
+            onChange={handleBaseSelected}
+            className="form-select appearance-none block w-6 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          >
+            <option value="value" selected>
+              Choose currrency
+            </option>
+            {coins.map((coin) => (
+              <option
+                className="text-black"
+                key={coin.asset_id}
+                value={coin.asset_id}
+              >
+                {coin.name}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selected}
+            name="selected"
+            id="selected"
+            onChange={handleSelected}
+            className="form-select appearance-none block w-6 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          >
+            <option value="value" selected>
+              Choose currrency
+            </option>
+            {coins.map((coin) => (
+              <option
+                className="text-black"
+                key={coin.asset_id}
+                value={coin.asset_id}
+              >
+                {coin.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-row justify-center gap-2">
+          {baseSelected &&
+            baseSelectedCoins.map((coin) => {
+              return (
+                <Coin
+                  key={coin.asset_id}
+                  name={coin.name}
+                  dataQuoteStart={coin.data_quote_start}
+                  dataQuoteEnd={coin.data_quote_end}
+                  dataTradeStart={coin.data_trade_start}
+                  dataTradeEnd={coin.data_trade_end}
+                  dataSymbolsCount={coin.data_symbols_count}
+                  volumeDay={coin.volume_1day_usd}
+                  volumeHour={coin.volume_1hrs_usd}
+                  volumeMonth={coin.volume_1mth_usd}
+                  price={coin.price_usd}
+                  dataStart={coin.data_start}
+                  dataEnd={coin.data_end}
+                />
+              );
+            })}
+          {selectedCoins &&
+            selectedCoins.map((coin) => {
+              return (
+                <Coin
+                  key={coin.asset_id}
+                  name={coin.name}
+                  dataQuoteStart={coin.data_quote_start}
+                  dataQuoteEnd={coin.data_quote_end}
+                  dataTradeStart={coin.data_trade_start}
+                  dataTradeEnd={coin.data_trade_end}
+                  dataSymbolsCount={coin.data_symbols_count}
+                  volumeDay={coin.volume_1day_usd}
+                  volumeHour={coin.volume_1hrs_usd}
+                  volumeMonth={coin.volume_1mth_usd}
+                  price={coin.price_usd}
+                  dataStart={coin.data_start}
+                  dataEnd={coin.data_end}
+                />
+              );
+            })}
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <h1 className="text-3xl mr-4">Search currency</h1>
         <form action="">
           <input
             type="text"
@@ -47,7 +149,7 @@ function App() {
             id="search"
             placeholder="Search"
             onChange={handleSearch}
-            className="block text-gray-700 ml-4 p-2"
+            className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
           />
         </form>
       </div>
