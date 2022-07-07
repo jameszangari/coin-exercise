b66708b133a46e2492e23313f9b20df79d647371
James Cliff <me@jamescliff.com>
2022-07-07 06:19:52 -0400

    feat: init repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Coin from "./Coin";
 
 function App() {
+  const [coins, setCoins] = useState<any[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `http://rest-sandbox.coinapi.io/v1/assets?apikey=${process.env.REACT_APP_COINAPI}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setCoins(data);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+  console.log(coins);
+
+  const [search, setSearch] = useState("");
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.currentTarget.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (isLoading)
+    return (
+      <div className="flex flex-row justify-center items-center uppercase">
+        <p className="mr-2">Loading</p>
+      </div>
+    );
+  if (!coins) return <p className="text-center">No data</p>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-4 max-w-7xl mx-auto">
+      <div className="flex flex-row items-center">
+        <h1 className="text-xl">Search currency</h1>
+        <form action="">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search"
+            onChange={handleSearch}
+            className="block text-gray-700 ml-4 p-2"
+          />
+        </form>
+      </div>
+      <div className="flex flex-row flex-wrap justify-center gap-2">
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.asset_id}
+              name={coin.name}
+              dataQuoteStart={coin.data_quote_start}
+              dataQuoteEnd={coin.data_quote_end}
+              dataTradeStart={coin.data_trade_start}
+              dataTradeEnd={coin.data_trade_end}
+              dataSymbolsCount={coin.data_symbols_count}
+              volumeDay={coin.volume_1day_usd}
+              volumeHour={coin.volume_1hrs_usd}
+              volumeMonth={coin.volume_1mth_usd}
+              price={coin.price_usd}
+              dataStart={coin.data_start}
+              dataEnd={coin.data_end}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Coin.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+export default function Coin({
+  name,
+  dataQuoteStart,
+  dataQuoteEnd,
+  dataTradeStart,
+  dataTradeEnd,
+  dataSymbolsCount,
+  volumeDay,
+  volumeHour,
+  volumeMonth,
+  price,
+  dataStart,
+  dataEnd,
+}) {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <button
+      className="w-96 my-4 border border-gray-500 rounded-md"
+      onClick={() => setToggle(!toggle)}
+    >
+      <div className="p-4">
+        <div className="item">
+          <h1 className="text-2xl font-bold">{name}</h1>
+          {price && <p className="mt-4">Price USD: ${price}</p>}
+        </div>
+        <div className="item">
+          <h1 className="mt-4 underline">Volume</h1>
+          <p>
+            <b>Day</b>: {volumeDay}
+          </p>
+          <p>
+            <b>Hour</b>: {volumeHour}
+          </p>
+          <p>
+            <b>Month</b>: {volumeMonth}
+          </p>
+        </div>
+      </div>
+      {toggle && (
+        <div>
+          <p>Data Quote Start: {dataQuoteStart}</p>
+          <p>Data Quote End: {dataQuoteEnd}</p>
+          <p>Data Trade Start: {dataTradeStart}</p>
+          <p>Data Trade End: {dataTradeEnd}</p>
+          <p>Data Symbols Count: {dataSymbolsCount}</p>
+          <p>Data Start: {dataStart}</p>
+          <p>Data End: {dataEnd}</p>
+        </div>
+      )}
+    </button>
+  );
+}
